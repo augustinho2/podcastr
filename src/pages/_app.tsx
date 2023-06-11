58d40494641ac9590af2ff9d36bd1ff87fdc7797
augustinho2 <augusto_guimaraes@gec.inatel.br>
2023-06-11 09:57:07 -0300

    app with the home layout done

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { Header } from '../components/Header'
+import '../styles/global.scss'
+import styles from '../styles/app.module.scss'
+import type { AppProps } from 'next/app'
+import { Player } from '../components/Player'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
+}
